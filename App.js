406ec5b47fdb5323234ab6bba10f8796b6fcb2fc
Dec1992 <deaglanburns@msn.com>
2021-05-31 15:59:01 +0100

    Modal

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { View } from "react-native";
 
+import AppPicker from "./app/components/AppPicker";
+import AppTextInput from "./app/components/AppTextInput";
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingScreen from "./app/screens/ListingScreen";
 
 export default function App() {
-  return <ListingScreen />;
+  return (
+    <Screen>
+      <AppTextInput icon="email" placeholder="email" />
+      <AppPicker icon="apps" placeholder="category" />
+    </Screen>
+  );
 }
